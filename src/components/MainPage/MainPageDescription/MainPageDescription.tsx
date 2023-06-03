88433f8140137153cffb1674a3e380597153bf0a
oxxitocina <U2HDxO@protonmail.com>
2023-06-03 16:33:01 +0600

    removed mock pictures

--- src/components/MainPage/MainPageDescription/MainPageDescription.tsx
+++ src/components/MainPage/MainPageDescription/MainPageDescription.tsx
@@ -3,7 +3,7 @@ import Typography from "@mui/material/Typography";
 export default function MainPageDescription({ description }) {
   return (
     <>
-      <Typography variant="h6" component="h4" sx={{ color: "#782abb" }}>
+      <Typography variant="h5" component="h4" sx={{ color: "#ffffff" }}>
         Описание
       </Typography>
       <Typography>{description}</Typography>
